eb641767df33b11ebf8d856d2e0830e90e1fa09b
Ming-Shih <minstone@gmail.com>
2023-11-24 00:36:05 -0500

    change port

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 // dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 
-console.log(process.env.MONGODB_URL)
+// console.log(process.env.MONGODB_URL)
 
 const corsOptions = {
 	origin: 'https://deis-evaluation.onrender.com',
@@ -32,21 +32,22 @@ app.use(morgan('dev'));
 // app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 /**
  * Set up mongodb connection and start the server
  */
-mongoose.
-	connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/course-eval', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log(`mongodb is connected on location: ${mongoose.connection.host}:${mongoose.connection.port}`);
-		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-	})
-	.catch((err) => {
-		console.log(`mongodb connection failed ${err}`);
-	});
+// mongoose.
+// 	connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/course-eval', {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	})
+// 	.then(() => {
+// 		console.log(`mongodb is connected on location: ${mongoose.connection.host}:${mongoose.connection.port}`);
+// 		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+// 	})
+// 	.catch((err) => {
+// 		console.log(`mongodb connection failed ${err}`);
+// 	});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
